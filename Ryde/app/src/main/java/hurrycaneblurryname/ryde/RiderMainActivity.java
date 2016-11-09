968503f9ac85319f131aeaef86ae2a951eee3db5
Chen Jiang <cjiang2@ualberta.ca>
2016-11-09 15:19:14 -0700

    set-up main_activities for rider and driver, class for signup and login

--- Ryde/app/src/main/java/hurrycaneblurryname/ryde/RiderMainActivity.java
+++ Ryde/app/src/main/java/hurrycaneblurryname/ryde/RiderMainActivity.java
@@ -3,11 +3,11 @@ package hurrycaneblurryname.ryde;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignupActivity extends AppCompatActivity {
+public class RiderMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_rider_main);
     }
 }
---
+++ Ryde/app/src/main/java/hurrycaneblurryname/ryde/View/EditUserProfile.java
@@ -0,0 +1,23 @@
+package hurrycaneblurryname.ryde.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import hurrycaneblurryname.ryde.R;
+
+public class EditUserProfile extends AppCompatActivity {
+    private EditText userEditText;
+    private EditText emailEditText;
+    private EditText phoneEditText;
+    private Button finishButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+        setTitle(R.string.editTitle);
+    }
+}
